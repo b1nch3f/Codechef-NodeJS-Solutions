621268722340247c9eacde6807e09806f1d02dda
sangram kesari ray <shankar.ray030@gmail.com>
2016-08-19 01:24:31 +0530

    initial commit

---
+++ TEST/README.md
@@ -0,0 +1,28 @@
+# NodeJS-Codechef-Template
+
+Inspired from webpack solutions used by 
+## rw251
+
+### https://www.codechef.com/users/rw251
+
+
+
+## Changelog
+### Added ES6 support
+
+* As this will be a webpack build, so it'll allow the user to freely include npm modules
+* Please make a note of the source file size limit in Codechef, as the final build can be very large
+
+## Instructions
+* Instal Node https://nodejs.org/en/
+* clone this repo [git clone https://github.com/shankar-ray/Codechef-NodeJS-Starter-Template.git]
+* In the working directory run npm install && npm install -g webpack
+* run webpack
+* main.js is the output file to be submitted to codechef
+
+### main.js is the solution for TEST [https://www.codechef.com/problems/TEST]
+### init.js has the main logic
+### to learn more, read about NodeJS, npm, webpack, babel and of course Javscript
+
+
+
---
+++ TEST/main.js
@@ -0,0 +1,42 @@
+(function() {
+	// start processing user input
+	process.stdin.resume();
+	process.stdin.setEncoding('ascii');
+	// declare global variables
+	var input_stdin = "";
+	var input_stdin_array = "";
+	var input_currentline = 0;
+	// standard input is stored into input_stdin
+	process.stdin.on('data', function (data) {
+	    input_stdin += data;
+	});
+	// standard input is done and stored into an array
+	process.stdin.on('end', function () {
+	    input_stdin_array = input_stdin.split("\n");
+	    main();    
+	});
+	// reads a line from the standard input array
+	function readLine() {
+	    return input_stdin_array[input_currentline++];
+	}
+
+	function solution(a, b) {
+	  var n = a;
+	  var k = b;
+	  var res = 0;
+	  for(var index = 0; index < n; index++) {
+	    if(parseInt(readLine()) % k === 0) { 
+	      res++;
+	    }
+	  }
+	  return res;  
+	}
+
+	function main() {
+		var n_k = readLine().split(' ');
+		var a = parseInt(n_k[0]);
+		var b = parseInt(n_k[1]);
+	  	var res = solution(a, b);
+	  	console.log(res);
+	}
+})();
